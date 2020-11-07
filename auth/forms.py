23e32019f7c21d8f763b16586244f16269cc304f
chlyoo <changhyunlyoo@gmail.com>
2020-11-07 18:46:03 +0900

    eng to kor

--- auth/forms.py
+++ auth/forms.py
@@ -7,30 +7,30 @@ from app import db
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+    email = StringField('이메일', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('비밀번호', validators=[DataRequired()])
+    remember_me = BooleanField('로그인 유지')
+    submit = SubmitField('로그인')
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    email = StringField('이메일', validators=[DataRequired(), Length(1, 64), Email()])
 
-    name = StringField('Username',
+    name = StringField('이름',
                        validators=[DataRequired(), Length(1, 64), Regexp('^[가-힣A-Za-z][가-힣A-Za-z0-9_.]*$', 0,
                                                                              'Usernames must have only letters, '
                                                                              'numbers, dots or underscores')])
-    password = PasswordField('Password', validators=[DataRequired(),
+    password = PasswordField('비밀번호', validators=[DataRequired(),
                                                      EqualTo('password2',
                                                              message='Passwords must match.')])
-    password2 = PasswordField('Confirm password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    password2 = PasswordField('비밀번호 확인', validators=[DataRequired()])
+    submit = SubmitField('회원가입')
 
     def validate_email(self, field):
         collection = db.get_collection('users')
         results = collection.find_one({'id': field.data})
         if results is not None:
-            raise ValidationError('Email already registered.')
+            raise ValidationError('이미 등록된 이메일 입니다.')
         pass
 
     # def validate_username(self, field):
