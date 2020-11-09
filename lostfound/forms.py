e9c83839193e5e8f30a11d3d787555e24447d3cc
chlyoo <changhyunlyoo@gmail.com>
2020-11-09 14:41:34 +0900

    adsense"

--- lostfound/forms.py
+++ lostfound/forms.py
@@ -24,7 +24,7 @@ class FoundForm(FlaskForm):
     place = StringField('습득장소')  # 이부분은 지도에서 핀을 설정해서 좌표를 가져와야 함
     found_date = DateTimeField('발견일시')
     detail = StringField('물건의 특징')
-    text = Label('ownership',text='소유권 주장')
+    text = Label('test', '소유권 주장')
     claim_ownership = RadioField(label='ownership', choices=[(True, '네'), (False, '아니오')], default=False)
     submit = SubmitField('등록')
     #TODO: 사진필드 추
